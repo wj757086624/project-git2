3cfb718a44c5f5aa673dfe803f90ff2ac7bd146e
wangjun <757086624@qq.com>
2019-06-02 21:37:45 +0800

    第二次提交修改内容

---
+++ project/src/com/zhx/Hello.java
@@ -0,0 +1,11 @@
+package com.zhx;
+
+/**
+ * Created by w'j on 2019/6/2.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("第一个git项目");
+    }
+}
